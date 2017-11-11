982d3ce031c92b40b02922c53a40c1ffbe8ea21a
yuuma30 <reon.maple10@gmai.com>
2017-11-11 14:33:26 +0900

    ViewFiles Initialize

---
+++ app/src/main/java/Kure/guraburu_kyuen_tool/OfficialTwitterFragment.java
@@ -0,0 +1,10 @@
+package Kure.guraburu_kyuen_tool;
+
+/**
+ * 公式ツイッターページ
+ *
+ * Created by yuuma on 2017/11/11.
+ */
+
+public class OfficialTwitterFragment {
+}
---
+++ app/src/main/java/Kure/guraburu_kyuen_tool/WikiFragment.java
@@ -0,0 +1,10 @@
+package Kure.guraburu_kyuen_tool;
+
+/**
+ * Wikiページ
+ *
+ * Created by yuuma on 2017/11/11.
+ */
+
+public class WikiFragment {
+}
