91e80550f69ba4b86e571f0fe5cafa690cc117d1
yuuma30 <reon.maple10@gmai.com>
2017-11-11 12:36:07 +0900

    Initialize

---
+++ app/src/main/java/Kure/guraburu_kyuen_tool/Config/Config.java
@@ -0,0 +1,12 @@
+package Kure.guraburu_kyuen_tool.Config;
+
+/**
+ * 設定変数やURLなどの変数を格納するのに利用
+ *
+ * Created by yuuma on 2017/11/11.
+ */
+
+public class Config {
+    // こんなかんじ？
+    public final String USER_NAME = "hogehoge";
+}
